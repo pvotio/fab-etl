1742c6d3736e5a28f3e0a3dcc187157988f58bee
Ali Moghimi <mghmiali@gmail.com>
2024-09-14 17:24:06 +0000

    fix: add timestamp

--- transformer/agent.py
+++ transformer/agent.py
@@ -1,6 +1,11 @@
+import datetime
+
 import pandas as pd
 
 
 def transform(parsed_data):
     df = pd.DataFrame(parsed_data)
+    df["timestamp_created_utc"] = datetime.datetime.utcnow()
+    if df.empty:
+        raise ValueError("Dataframe is empty. Insertion terminated.")
     return df
